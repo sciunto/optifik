157fcdc10248742ae5b67c32d5c808cd00c41b7c
François Boulogne <devel@sciunto.org>
2025-06-20 11:01:44 +0200

    refactoring

--- tests/test_SV2o0.py
+++ tests/test_SV2o0.py
@@ -3,7 +3,8 @@ import numpy as np
 from numpy.testing import assert_allclose
 import pytest
 
-from optifik.scheludko import thickness_for_order0 
+from optifik.scheludko import thickness_from_scheludko
+from optifik.scheludko import get_default_start_stop_wavelengths
 from optifik.io import load_spectrum
 from optifik.analysis import smooth_intensities
 
@@ -26,14 +27,23 @@ def test_SV2o0_small_tol(spectrum_path, expected):
     lambdas, raw_intensities = load_spectrum(spectrum_path, lambda_min=450)
     smoothed_intensities = smooth_intensities(raw_intensities)
 
-    refractive_index =  1.324188 + 3102.060378 / (lambdas**2)
+    r_index =  1.324188 + 3102.060378 / (lambdas**2)
     prominence = 0.020
 
+    w_start, w_stop = None, None
+    #w_start, w_stop = get_default_start_stop_wavelengths(lambdas,
+    #                                                     smoothed_intensities,
+    #                                                     refractive_index=r_index,
+    #                                                     min_peak_prominence=prominence,
+    #                                                     plot=False)
 
-    result = thickness_for_order0(lambdas, smoothed_intensities,
-                                               refractive_index=refractive_index,
-                                               min_peak_prominence=prominence,
-                                               plot=False)
+    result = thickness_from_scheludko(lambdas,
+                                      smoothed_intensities,
+                                      refractive_index=r_index,
+                                      wavelength_start=w_start,
+                                      wavelength_stop=w_stop,
+                                      interference_order=0,
+                                      plot=False)
 
     assert_allclose(result.thickness, expected, rtol=1e-1)
 
@@ -42,14 +52,23 @@ def test_SV2o0_large_tol(spectrum_path, expected):
     lambdas, raw_intensities = load_spectrum(spectrum_path, lambda_min=450)
     smoothed_intensities = smooth_intensities(raw_intensities)
 
-    refractive_index =  1.324188 + 3102.060378 / (lambdas**2)
+    r_index =  1.324188 + 3102.060378 / (lambdas**2)
     prominence = 0.020
 
+    w_start, w_stop = None, None
+#    w_start, w_stop = get_default_start_stop_wavelengths(lambdas,
+#                                                         smoothed_intensities,
+#                                                         refractive_index=r_index,
+#                                                         min_peak_prominence=prominence,
+#                                                         plot=False)
 
-    result = thickness_for_order0(lambdas, smoothed_intensities,
-                                               refractive_index=refractive_index,
-                                               min_peak_prominence=prominence,
-                                               plot=False)
+    result = thickness_from_scheludko(lambdas,
+                                      smoothed_intensities,
+                                      refractive_index=r_index,
+                                      wavelength_start=w_start,
+                                      wavelength_stop=w_stop,
+                                      interference_order=0,
+                                      plot=False)
 
     assert_allclose(result.thickness, expected, rtol=2.5e-1)
 
