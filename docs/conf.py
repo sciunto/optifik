39067bd588e2fbb40925392c576aeac05895a3a0
François Boulogne <devel@sciunto.org>
2025-06-25 13:48:07 +0200

    add test notebook

--- docs/conf.py
+++ docs/conf.py
@@ -21,15 +21,20 @@ html_baseurl = "https://sciunto.github.io/optifik/"
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'numpydoc',  # Doit être après 'autodoc'
+    'numpydoc',
     'myst_parser',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
+    'nbsphinx',
+    'sphinx.ext.mathjax',
 ]
 
 numpydoc_show_inherited_class_members = True
 numpydoc_use_plots = False  # Plots in doctring
 numpydoc_class_members_toctree = False
+
+nbsphinx_execute = 'auto'  # ou 'auto' pour exécuter si nécessaire
+nbsphinx_notebooks = ['notebooks/']  # Dossier contenant vos .ipynb
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
